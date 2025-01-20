b12dc8fe832d421f64511eca1eca211ff65960e4
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2025-01-21 02:02:44 +0900

    メインクラスを org.example.App から jp.ac.uryukyu.ie.e245735.Main に変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.App"
+    mainClass = "jp.ac.uryukyu.ie.e245735.Main"
 }
 
 tasks.named<Test>("test") {
