b53e96fcbf68ad111c32aacc9a82b6f7561a5108
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2025-01-21 10:27:10 +0900

    Mainクラスにmainメソッドを追加し、設定の初期化処理を実装

--- app/src/main/java/jp/ac/uryukyu/ie/e245735/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245735/Main.java
@@ -1,5 +1,8 @@
 package jp.ac.uryukyu.ie.e245735;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Setting setting = new Setting();
+        setting.init();
+    }
 }
