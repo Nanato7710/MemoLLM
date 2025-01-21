54875a94b4124bddb894782111a41426547595a0
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2025-01-21 11:34:49 +0900

    LLMクラスにモデルの初期化と生成メソッドを追加し、パラメータの取得・設定メソッドを実装

--- app/src/main/java/jp/ac/uryukyu/ie/e245735/LLM.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245735/LLM.java
@@ -1,5 +1,42 @@
 package jp.ac.uryukyu.ie.e245735;
 
+import de.kherud.llama.InferenceParameters;
+import de.kherud.llama.LlamaIterable;
+import de.kherud.llama.LlamaModel;
+import de.kherud.llama.ModelParameters;
+import de.kherud.llama.args.MiroStat;
+import jp.ac.uryukyu.ie.e245735.params.LLMParams;
+
 public class LLM {
-    
+    private LLMParams params;
+    private LlamaModel model;
+
+    public LLM(LLMParams params) {
+        this.params = params;
+        ModelParameters modelParams = new ModelParameters()
+                                        .setModelFilePath(params.getModelPath())
+                                        .setNGpuLayers(params.getNGpulayers());
+        model = new LlamaModel(modelParams);
+    }
+
+    public LlamaIterable generate(String prompt) {
+        InferenceParameters infParams = new InferenceParameters(prompt)
+                                            .setTemperature(params.getTemperature())
+                                            .setStopStrings(params.getStopString())
+                                            .setPenalizeNl(true)
+                                            .setMiroStat(MiroStat.V2);
+        return model.generate(infParams);
+    }
+
+    public void close() {
+        model.close();
+    }
+
+    public LLMParams getParams() {
+        return params;
+    }
+
+    public void setParams(LLMParams params) {
+        this.params = params;
+    }
 }
