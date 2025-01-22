b71e70428ea8d2e8c08d040fdb73a94cf0ca485e
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2025-01-22 12:43:05 +0900

    MemoParamsクラスにcontentsフィールドを追加し、ファイルから内容を読み込むgetContentsメソッドを実装

--- app/src/main/java/jp/ac/uryukyu/ie/e245735/params/MemoParams.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245735/params/MemoParams.java
@@ -1,9 +1,12 @@
 package jp.ac.uryukyu.ie.e245735.params;
 
+import java.io.File;
+
 import jp.ac.uryukyu.ie.e245735.Setting;
 
 public class MemoParams {
     private String fileName;
+    private String contents;
 
     public String getFileName() {
         return fileName;
@@ -11,5 +14,19 @@ public class MemoParams {
 
     public String getFilePath() {
         return Setting.workingDir + Setting.sep + fileName;
+    }
+
+    public String getContents() {
+        if (contents == null) {
+            File file = new File(getFilePath());
+            if (file.exists()) {
+                contents = Setting.readFile(file);
+            }
+        }
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
     }
 }
