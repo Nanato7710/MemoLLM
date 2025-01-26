03458599565cdbf282d357e4eda141a3e00572a6
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2025-01-26 22:16:36 +0900

    ChatTemplateクラスにstructuringPromptフィールドを追加し、コンストラクタとconvertToInputメソッドを修正。構造化メモ用のgetPromptForStructuringMemoメソッドを実装。

--- app/src/main/java/jp/ac/uryukyu/ie/e245735/ChatTemplate.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245735/ChatTemplate.java
@@ -9,10 +9,12 @@ public class ChatTemplate {
     private ArrayList<String> chatHistory = new ArrayList<>();
     private ArrayList<String> chatHistoryForUser = new ArrayList<>();
     private String systemPrompt;
+    private String structuringPrompt;
 
-    public ChatTemplate(ChatTemplateParams params, String systemPrompt) {
+    public ChatTemplate(ChatTemplateParams params, String systemPrompt, String structuringPrompt) {
         this.params = params;
         this.systemPrompt = systemPrompt;
+        this.structuringPrompt = structuringPrompt;
     }
 
     public void addUserChat(String chat) {
@@ -35,8 +37,8 @@ public class ChatTemplate {
         return sb.toString();
     }
 
-    public String convertToInput() {
-            return toString() + params.getSuffix();
+    public String convertToInput(String structuredMemo) {
+        return toString() + String.format("<|start_header_id|>information<|end_header_id|>\\n%s<|eot_id|>",structuredMemo) + params.getSuffix();
     }
 
     public String getChatHistoryForUser() {
@@ -44,6 +46,13 @@ public class ChatTemplate {
         for (String chat : chatHistoryForUser) {
             sb.append(chat + "\n");
         }
+        return sb.toString();
+    }
+
+    public String getPromptForStructuringMemo(String memo) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format(params.getSystem(), structuringPrompt));
+        sb.append(String.format(params.getUser(), memo));
         return sb.toString();
     }
 }
