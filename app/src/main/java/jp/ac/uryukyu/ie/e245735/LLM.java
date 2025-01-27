b5225867b0298b8189894f483eee12f53002f87d
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2025-01-27 20:15:52 +0900

    LLMクラスにJavadocコメントを追加

--- app/src/main/java/jp/ac/uryukyu/ie/e245735/LLM.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245735/LLM.java
@@ -10,6 +10,14 @@ import de.kherud.llama.ModelParameters;
 import de.kherud.llama.args.MiroStat;
 import jp.ac.uryukyu.ie.e245735.params.LlmParameters;
 
+/**
+ * Llm
+ * 
+ * LLMを利用するクラス
+ * 
+ * @param params LlmParameters
+ * @param model LlamaModel
+ */
 public class Llm {
     private LlmParameters params;
     private LlamaModel model;
@@ -20,6 +28,9 @@ public class Llm {
         initializeModel();
     }
 
+    /**
+     * モデルファイルが存在しない場合はダウンロード
+     */
     private void downloadModelIfAbsent() {
         File modelFile = new File(params.getModelPath());
         if (!modelFile.exists()) {
@@ -31,6 +42,9 @@ public class Llm {
         }
     }
 
+    /**
+     * モデルを初期化
+     */
     private void initializeModel() {
         ModelParameters modelParams = new ModelParameters()
                 .setModelFilePath(params.getModelPath())
@@ -38,11 +52,23 @@ public class Llm {
         model = new LlamaModel(modelParams);
     }
 
+    /**
+     * テキストを生成
+     * 
+     * @param prompt プロンプト
+     * @return LlamaIterable
+     */
     public LlamaIterable generate(String prompt) {
         InferenceParameters infParams = createInferenceParams(prompt);
         return model.generate(infParams);
     }
 
+    /**
+     * テキストを生成するためのLLMのパラメータを作成
+     * 
+     * @param prompt プロンプト
+     * @return LlamaIterable
+     */
     private InferenceParameters createInferenceParams(String prompt) {
         return new InferenceParameters(prompt)
                 .setTemperature(params.getTemperature())
@@ -51,14 +77,27 @@ public class Llm {
                 .setMiroStat(MiroStat.V2);
     }
 
+    /**
+     * モデルをクローズ
+     */
     public void close() {
         model.close();
     }
 
+    /**
+     * パラメータを取得する
+     * 
+     * @return LlmParameters
+     */
     public LlmParameters getParams() {
         return params;
     }
 
+    /**
+     * パラメータを設定する
+     * 
+     * @param params LlmParameters
+     */
     public void setParams(LlmParameters params) {
         this.params = params;
     }
