498cb5130789e658a8388751ef0e3d25ade20e2f
Kazusa KINJO <e245735@ie.u-ryukyu.ac.jp>
2025-01-27 19:43:33 +0900

    MemoParametersクラスにコンストラクタとJavadocを追加

--- app/src/main/java/jp/ac/uryukyu/ie/e245735/params/MemoParameters.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245735/params/MemoParameters.java
@@ -4,23 +4,55 @@ import java.io.File;
 
 import jp.ac.uryukyu.ie.e245735.AppSetting;
 
+/**
+ * MemoParameters
+ * 
+ * メモのパラメータを保持するクラス
+ * 
+ * @param fileName ファイル名
+ * @param contents メモの内容
+ */
 public class MemoParameters {
     private String fileName;
     private String contents;
 
+    public MemoParameters(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * ファイル名を取得する
+     * 
+     * @return ファイル名
+     */
     public String getFileName() {
         return fileName;
     }
 
+    /**
+     * ファイル名を設定する
+     * 
+     * @param fileName ファイル名
+     */
     public String getFilePath() {
         return AppSetting.workingDir + AppSetting.sep + fileName;
     }
 
+    /**
+     * メモの内容を取得する
+     * 
+     * @return メモの内容
+     */
     public String getContents() {
         loadContentsIfNeeded();
         return contents;
     }
 
+    /**
+     * メモの内容をファイルから取得して、contentsにセットする
+     * 
+     * @param contents メモの内容
+     */
     private void loadContentsIfNeeded() {
         if (contents == null) {
             File file = new File(getFilePath());
@@ -30,6 +62,11 @@ public class MemoParameters {
         }
     }
 
+    /**
+     * メモの内容を設定する
+     * 
+     * @param contents メモの内容
+     */
     public void setContents(String contents) {
         this.contents = contents;
     }
